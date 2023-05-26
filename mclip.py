ec7ab6fb4f14157c5064dfa521fb79ee18e921f4
pranavshirali <pranavshirali007@gmail.com>
2023-05-26 09:57:19 +0530

    Update mclip.py

--- mclip.py
+++ mclip.py
@@ -5,19 +5,19 @@
 '''
 Description:
 
-    1.The script imports two modules: sys and pyperclip. sys is a standard Python 
+    ->1.The script imports two modules: sys and pyperclip. sys is a standard Python 
         module for system-specific parameters and functions, while pyperclip is a third-party module used for accessing the clipboard.
 
-    2.The program defines a dictionary called text, which associates keyphrases (keys) with corresponding text snippets (values). 
+    ->2.The program defines a dictionary called text, which associates keyphrases (keys) with corresponding text snippets (values). 
         This dictionary serves as the storage for your predefined texts.
 
-    3.The script checks the command-line arguments provided when running the program. 
+    ->3.The script checks the command-line arguments provided when running the program. 
         If the number of arguments is less than 2 (only the script name itself), it prints an error message and exits.
 
-    4.If the keyphrase provided as an argument exists in the text dictionary, the corresponding text snippet 
+    ->4.If the keyphrase provided as an argument exists in the text dictionary, the corresponding text snippet 
         is copied to the clipboard using pyperclip.copy(). It also prints a message confirming that the text has been copied.
 
-    5.If the keyphrase doesn't exist in the text dictionary, it prints a message indicating that the keyphrase is not available.
+    ->5.If the keyphrase doesn't exist in the text dictionary, it prints a message indicating that the keyphrase is not available.
 
 '''
 
